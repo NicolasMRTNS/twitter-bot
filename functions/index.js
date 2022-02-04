d7b3ba983014f2168f6b4ca5ad4827a0d1a9853a
Nicolas Martins <nicolas.martins@hotmail.fr>
2022-02-04 21:48:08 +0100

    fully functionnal, just need a cron to automate it and hosting

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 require('dotenv').config();
+const { Configuration, OpenAIApi } = require('openai');
 
 admin.initializeApp();
 
@@ -14,19 +15,26 @@ const twitterClient = new twitterApi({
 
 const callbackUrl = 'http://127.0.0.1:5000/twitter-bot-e52b4/us-central1/callback';
 
-exports.auth = functions.https.onRequest((request, response) => {
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+exports.auth = functions.https.onRequest(async (_request, response) => {
   const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(callbackUrl, {
     scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access']
   });
 
-  dbRef.set({ codeVerifier, state });
+  await dbRef.set({ codeVerifier, state });
 
   response.redirect(url);
 });
 
-exports.callback = functions.https.onRequest((request, response) => {
+exports.callback = functions.https.onRequest(async (request, response) => {
   const { state, code } = request.query;
-  const dbSnapshot = dbRef.get();
+  const dbSnapshot = await dbRef.get();
   const { codeVerifier, state: storedState } = dbSnapshot.data();
 
   if (state !== storedState) {
@@ -44,4 +52,23 @@ exports.callback = functions.https.onRequest((request, response) => {
   response.sendStatus(200);
 });
 
-exports.tweet = functions.https.onRequest((request, response) => {});
+exports.tweet = functions.https.onRequest(async (_request, response) => {
+  const { refreshToken } = (await dbRef.get()).data();
+
+  const {
+    client: refreshedClient,
+    accessToken,
+    refreshToken: newRefreshToken
+  } = await twitterClient.refreshOAuth2Token(refreshToken);
+
+  await dbRef.set({ accessToken, refreshToken: newRefreshToken });
+
+  const nextTweet = await openai.createCompletion('text-davinci-001', {
+    prompt: 'Tweet something cool for #techtwitter',
+    max_tokens: 64
+  });
+
+  const { data } = await refreshedClient.v2.tweet(nextTweet.data.choices[0].text);
+
+  response.send(data);
+});
